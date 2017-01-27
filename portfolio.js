7bdae59df8179b662a845bb264ab7c8611b9717a
Caleb Pearce <cpearce31@gmail.com>
2017-01-27 14:06:34 -0500

    Added rewritten carousel for projects section.

--- portfolio.js
+++ portfolio.js
@@ -23,22 +23,6 @@ function main () {
         skills2.appendChild(childrenArr[i]);
       }
     }
-
-    // Show only one project
-    if (width <= 980) {
-      currentCenterProject = 0;
-      for (let i = 0; i < projects.length; i++) {
-        projects[i].style.display = 'none';
-      }
-      ticks[0].classList.add('tick-active');
-      projects[0].style.display = 'inline-flex';
-    } else {
-      currentCenterProject = 1;
-      for (let i = 0; i < 3; i++) {
-        projects[i].style.display = 'inline-flex';
-        ticks[i].classList.add('tick-active');
-      }
-    }
   }
 
   window.addEventListener('resize', resizeThrottler, false);
@@ -103,44 +87,120 @@ function main () {
   let skills2 = document.getElementById('skills2');
   let skillsWrapper = document.getElementById('skills-wrapper');
 
-  let right = document.getElementById('scroll-right');
-  let left = document.getElementById('scroll-left');
-  let projects = document.querySelectorAll('#projects-wrapper > div');
-  let ticks = document.querySelectorAll('.tick');
-  let currentCenterProject;
-
-  right.addEventListener('click', () => {
-    if (width <= 980 && currentCenterProject !== 6) {
-      projects[currentCenterProject].style.display = 'none';
-      projects[currentCenterProject + 1].style.display = 'inline-flex';
-      ticks[currentCenterProject].classList.toggle('tick-active');
-      ticks[currentCenterProject + 1].classList.toggle('tick-active');
-      currentCenterProject++;
-    } else if (width > 980 && currentCenterProject !== 5) {
-      projects[currentCenterProject - 1].style.display = 'none';
-      projects[currentCenterProject + 2].style.display = 'inline-flex';
-      ticks[currentCenterProject - 1].classList.toggle('tick-active');
-      ticks[currentCenterProject + 2].classList.toggle('tick-active');
-      currentCenterProject++;
-    }
-  });
-  left.addEventListener('click', () => {
-    if (width <= 980 && currentCenterProject !== 0) {
-      projects[currentCenterProject].style.display = 'none';
-      projects[currentCenterProject - 1].style.display = 'inline-flex';
-      ticks[currentCenterProject].classList.toggle('tick-active');
-      ticks[currentCenterProject - 1].classList.toggle('tick-active');
-      currentCenterProject--;
-    } else if (width > 980 && currentCenterProject !== 1) {
-      projects[currentCenterProject + 1].style.display = 'none';
-      projects[currentCenterProject - 2].style.display = 'inline-flex';
-      ticks[currentCenterProject + 1].classList.toggle('tick-active');
-      ticks[currentCenterProject - 2].classList.toggle('tick-active');
-      currentCenterProject--;
-    }
-  });
-
   resizeResponse();
+
+  var AUTOSCROLL = false; // Change to true to enable autoscrolling
+  var PERIOD = 5000;  // Time spent on each slide in milliseconds
+
+  // If touch events are detected, disable autoscrolling
+  var touchDevice = false;
+  window.addEventListener('touchstart', function () {
+    touchDevice = true;
+  });
+
+  var slides = document.querySelectorAll('.carousel-slide');
+  var left = document.querySelector('#carousel-btn-left');
+  var right = document.querySelector('#carousel-btn-right');
+  var indicator = document.querySelector('#carousel-scroll-indicator');
+  var wrapper = document.querySelector('#carousel-wrapper');
+
+  // Add bulvars to the scoll indicator section for each slide
+  for (var i = 0; i < slides.length; i++) {
+    var pip = document.createElement('i');
+    pip.className = 'carousel-pip fa fa-square';
+    pip.toggle = function () {
+      this.classList.toggle('carousel-pip-active');
+    };
+    indicator.appendChild(pip);
+  }
+
+  var index = 0;
+  var pips = document.querySelectorAll('.carousel-pip');
+  pips[0].toggle();
+
+  // Wrap-around handling for scroll-idicators (pips!)
+  function changePip (direction) {
+    pips[index].toggle();
+    if (direction === 'right') {
+      if (index === pips.length - 1) {
+        index = 0;
+      } else {
+        index++;
+      }
+    } else if (direction === 'left') {
+      if (index === 0) {
+        index = pips.length - 1;
+      } else {
+        index--;
+      }
+    }
+    pips[index].toggle();
+  }
+
+  // Remove last slide and insert it at the begining of the list,
+  // in case the first move is to the left
+  slides[slides.length - 1].remove();
+  setPosition(slides[slides.length - 1], 'left');
+  wrapper.insertBefore(slides[slides.length - 1], slides[0]);
+  slides = document.querySelectorAll('.carousel-slide');
+
+  function setPosition (slide, pos) {
+    if (pos === 'right') {
+      slide.className = 'carousel-slide carousel-slide-right';
+    } else if (pos === 'left') {
+      slide.className = 'carousel-slide carousel-slide-left';
+    } else {
+      slide.className = 'carousel-slide carousel-slide-active';
+    }
+  }
+
+  function move (direction) {
+    if (direction === 'right') {
+      setPosition(slides[1], 'left');
+      setPosition(slides[2]);
+      slides[0].remove();
+      setPosition(slides[0], 'right');
+      wrapper.appendChild(slides[0]);
+      slides = document.querySelectorAll('.carousel-slide');
+    } else if (direction === 'left') {
+      setPosition(slides[0]);
+      setPosition(slides[1], 'right');
+      slides[slides.length - 1].remove();
+      setPosition(slides[slides.length - 1], 'left');
+      wrapper.insertBefore(slides[slides.length - 1], slides[0]);
+      slides = document.querySelectorAll('.carousel-slide');
+    }
+    changePip(direction);
+  }
+
+  right.addEventListener('click', function () {
+    move('right');
+  });
+
+  left.addEventListener('click', function () {
+    move('left');
+  });
+
+  function startAutoScroll () {
+    if (AUTOSCROLL && !touchDevice) {
+      interval = setInterval(function () {
+        move('right');
+      }, PERIOD);
+    }
+  }
+
+  wrapper.addEventListener('mouseover', function () {
+    if (interval) {
+      clearInterval(interval);
+    }
+  });
+
+  wrapper.addEventListener('mouseout', function () {
+    startAutoScroll();
+  });
+
+  var interval;
+  startAutoScroll();
 }
 
 window.addEventListener('load', main, false);
